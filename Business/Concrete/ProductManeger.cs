28ac71ce811a62105f753d14852aeb1d3640dc09
eersoy <eersoy@DESKTOP-M93O1RS>
2021-01-31 00:33:45 +0300

    Add project files.

---
+++ Business/Concrete/ProductManeger.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManeger : IProductService
+    {
+        IProductDal _productDal;   //soyut nesneyle bağlantı kuruyoruz ne inmemory ne entity framework adı geçecek
+
+        public ProductManeger(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //Yetkisi var mı? gibi
+
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManeger productManeger = new ProductManeger(new InMemoryProductDal());
+
+            foreach (var product in productManeger.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            
+        }
+    }
+}
